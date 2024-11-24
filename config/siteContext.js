b0cfbf999e3ab7022cbce07f67d095a687d89c9d
cujumbu <nethandelskompagniet@gmail.com>
2024-11-24 16:16:19 +0100

    Create siteContext.js

---
+++ config/siteContext.js
@@ -0,0 +1,24 @@
+export const siteContext = {
+  mainTopic: 'Maritime',
+  context: `Focus on maritime-related content including:
+    - Naval History
+    - Navigation Systems
+    - Maritime Technology
+    - Marine Engineering
+    - Maritime Law
+    - Marine Science
+    - Vessel Types
+    - Maritime Education`,
+  relatedTopicsScope: [
+    'naval-history',
+    'navigation',
+    'maritime-technology',
+    'marine-engineering',
+    'maritime-law',
+    'marine-science',
+    'vessel-types',
+    'maritime-education',
+    'seamanship',
+    'oceanography'
+  ]
+};
